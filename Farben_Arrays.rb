0fe2fed58daa666e681cf1cd77434bd71e3cb863
Robert Schaefer <robert.schaefer@student.hpi.de>
2016-07-12 11:57:41 +0200

    refactor for readability

--- Farben_Arrays.rb
+++ Farben_Arrays.rb
@@ -6,11 +6,12 @@ puts  "Die Farbe #{gewaehlte_farbe} gefällt mir"
 position = farben.index(gewaehlte_farbe)
 
 # TODO: Was passiert hier, wenn der Benutzer Quatsch eingibt?
-linkernachbar_index = position - 1
-rechternachbar_index = position + 1
 
-  puts "Der linke Nachbar von #{gewaehlte_farbe} ist #{farben[linkernachbar_index] || "nicht vorhanden"}"
+linkernachbar = farben[position - 1]
+rechternachbar = farben[position + 1]
+
+puts "Der linke Nachbar von #{gewaehlte_farbe} ist #{linkernachbar || "nicht vorhanden"}"
   # || bedeutet "oder" nil und false= false wenn links bei farben "nil", dann greift das "oder" und Ausgabe:
   # nicht vorhanden
 
-  puts "Der rechte Nachbar von #{gewaehlte_farbe} ist #{farben[rechternachbar_index] || "nicht vorhanden"}"
+puts "Der rechte Nachbar von #{gewaehlte_farbe} ist #{rechternachbar || "nicht vorhanden"}"
